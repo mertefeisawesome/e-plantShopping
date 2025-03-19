dccf25a7ffeaf379f40d6da6cf0df31e1775ff61
Mert Efe <mertefeisawesome@gmail.com>
2025-03-20 01:07:04 +0300

    Cart icon updated with numbers

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,36 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total = 0;
+    cart.forEach((item) => {total += calculateTotalCost(item)});
+    return total;
  
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckOutShopping = (e) => {
+    alert("Functionality to be added later");
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+   } else {handleRemove(item);}
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-  };
+  const calculateTotalCost = (item) => item.quantity * parseFloat(item.cost.substring(1));
 
   return (
     <div className="cart-container">
@@ -57,7 +65,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckOutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -259,9 +259,9 @@ function ProductList({ onHomeClick }) {
 
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
-        setAddedToCart((prevState) => ({ ...prevState, [product.name]: true,}));
+        setAddedToCart((prevState) => ({ ...prevState, [product.name]: true}));
 
-    }
+    };
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -279,7 +279,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>{Object.keys(addedToCart).length > 0 ? Object.keys(addedToCart).length : ""}</h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -288,13 +288,15 @@ function ProductList({ onHomeClick }) {
                         <div key={index}>
                             <h2>{category.category}</h2>
                             <div className='product-list'>
-                                {category.plants.map((plant, plantIndex) => (
+                                {category.plants.map((plant, plantIndex) => {
+                                    const plantName = plant.name;
+                                    return (
                                     <div className='product-card' key={plantIndex}>
                                         <img className='product-image' src={plant.image} alt={plant.name} />
                                         <div className='product-title'>{plant.name}</div>
-                                        <button className='product-button' onClick={() => handleAddToCart(plant)}>Add To Cart</button>
+                                        <button className='product-button' onClick={() => handleAddToCart(plant)} disabled={addedToCart[plantName] ? true : false}>Add To Cart</button>
                                     </div>
-                                ))}
+                                )})}
                         </div></div>
                     ))}
                 </div>
